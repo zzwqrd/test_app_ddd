66d76630ef1626e4be88a31139f42770599938c5
zzwqrd <zeinelabdeen21@gmail.com>
2024-07-30 20:55:31 +0300

    add data and time faild

--- lib/features/user/presentation/view/view.dart
+++ lib/features/user/presentation/view/view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/utils/helpers/text_form_date.dart';
+import '../../../../core/utils/helpers/text_form_time_date.dart';
 
 class UserView extends StatelessWidget {
   UserView({super.key});
   final TextEditingController controller = TextEditingController();
+  final TextEditingController dateAndTimecontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +23,21 @@ class UserView extends StatelessWidget {
 
             // initDate: DateTime.now(),
             label: "Select Date and Time",
-            helperText: "Tap to select date and time",
+            // helperText: "Tap to select date and time",
             // icon: Icons.calendar_today,
             suffixIcon: Icons.access_time,
             borderLess: false,
+          ),
+          const SizedBox(height: 30),
+          DefaultDateAndTimeTextField(
+            controller: dateAndTimecontroller,
+            onDateTimeChange: (dateTime) {},
+            initDate: DateTime.now(),
+            label: "Select Date and Time",
+            // helperText: "Tap to select date and time",
+            // icon: Icons.calendar_today,
+            suffixIcon: Icons.access_time,
+            mode: DateTimeMode.time, // حدد الوضع هنا
           ),
         ],
       ),
