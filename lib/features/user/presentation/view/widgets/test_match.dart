f6b09b6600c1b8cdd211245e659e3027e9f28a2e
zzwqrd <zeinelabdeen21@gmail.com>
2024-07-31 22:32:12 +0300

    time match

--- lib/features/user/presentation/view/widgets/test_match.dart
+++ lib/features/user/presentation/view/widgets/test_match.dart
@@ -7,10 +7,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String content = 'محتوى يوم معين';
+  bool isLoading = false;
+
+  void _onDateChanged(String newContent) {
+    setState(() {
+      content = newContent;
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Date Selection Bar'),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          DateSelectionBar(onDateChanged: _onDateChanged),
+          Expanded(
+            child: Center(
+              child: isLoading
+                  ? CircularProgressIndicator()
+                  : AnimatedSwitcher(
+                      duration: Duration(milliseconds: 100),
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return FadeTransition(opacity: animation, child: child);
+                      },
+                      child: Text(
+                        content,
+                        key: ValueKey<String>(content),
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DateSelectionBar extends StatefulWidget {
+  final ValueChanged<String>? onDateChanged;
+
+  DateSelectionBar({this.onDateChanged});
+
+  @override
+  _DateSelectionBarState createState() => _DateSelectionBarState();
+}
+
+class _DateSelectionBarState extends State<DateSelectionBar> {
   late List<DateItem> dates;
   late int selectedIndex;
-  String content = 'محتوى يوم';
-  bool isLoading = false;
   bool isFirstLoad = true;
 
   final ScrollController _scrollController = ScrollController();
@@ -19,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     dates = _generateDatesForCurrentMonth();
-    selectedIndex = DateTime.now().day - 1; // تحديد اليوم الحالي كمؤشر تلقائي
+    selectedIndex = DateTime.now().day - 1;
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _scrollToSelectedIndex();
       _updateContent();
@@ -33,8 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     for (int i = 1; i <= daysInMonth; i++) {
       DateTime date = DateTime(now.year, now.month, i);
-      String dayName =
-          DateFormat('EEEE', 'ar').format(date); // عرض اسم اليوم بالعربية
+      String dayName = DateFormat('EEEE', 'ar').format(date);
       generatedDates.add(DateItem(dayName, i.toString()));
     }
 
@@ -43,37 +95,30 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _onDateSelected(int index) {
     setState(() {
-      isLoading = true;
       selectedIndex = index;
     });
 
-    // محاكاة عملية تحميل (مثل جلب بيانات من خادم)
     Future.delayed(Duration(seconds: 1), () {
-      setState(() {
-        isLoading = false;
-        _updateContent();
-        _scrollToSelectedIndex();
-      });
+      _updateContent();
+      _scrollToSelectedIndex();
     });
   }
 
   void _updateContent() {
-    setState(() {
-      content =
-          'محتوى يوم ${dates[selectedIndex].day} ${dates[selectedIndex].date}';
-    });
+    String newContent =
+        'محتوى يوم ${dates[selectedIndex].day} ${dates[selectedIndex].date}';
+    widget.onDateChanged?.call(newContent);
   }
 
   void _scrollToSelectedIndex() {
     if (isFirstLoad) {
-      double position =
-          selectedIndex * 60.0; // تحديد الموقع المناسب للتاريخ المحدد
+      double position = selectedIndex * 60.0;
       _scrollController.animateTo(
         position,
         duration: Duration(milliseconds: 300),
         curve: Curves.easeInOut,
       );
-      isFirstLoad = false; // تعيين isFirstLoad إلى false بعد التمرير الأولي
+      isFirstLoad = false;
     }
   }
 
@@ -95,84 +140,25 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Date Selection Bar'),
-        backgroundColor: Colors.black,
-      ),
-      body: Column(
-        children: [
-          DateSelectionBar(
-            dates: dates,
-            selectedIndex: selectedIndex,
-            onDateSelected: _onDateSelected,
-            scrollController: _scrollController,
-            scrollLeft: _scrollLeft,
-            scrollRight: _scrollRight,
-          ),
-          Expanded(
-            child: Center(
-              child: isLoading
-                  ? CircularProgressIndicator()
-                  : AnimatedSwitcher(
-                      duration: Duration(milliseconds: 300),
-                      transitionBuilder:
-                          (Widget child, Animation<double> animation) {
-                        return FadeTransition(opacity: animation, child: child);
-                      },
-                      child: Text(
-                        content,
-                        key: ValueKey<String>(content),
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class DateSelectionBar extends StatelessWidget {
-  final List<DateItem> dates;
-  final int selectedIndex;
-  final Function(int) onDateSelected;
-  final ScrollController scrollController;
-  final Function scrollLeft;
-  final Function scrollRight;
-
-  DateSelectionBar({
-    required this.dates,
-    required this.selectedIndex,
-    required this.onDateSelected,
-    required this.scrollController,
-    required this.scrollLeft,
-    required this.scrollRight,
-  });
-
-  @override
-  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20.0),
       child: Row(
         children: [
           IconButton(
             icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => scrollLeft(),
+            onPressed: () => _scrollLeft(),
           ),
           Expanded(
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              controller: scrollController,
+              controller: _scrollController,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: dates
                     .asMap()
                     .entries
                     .map((entry) => GestureDetector(
-                          onTap: () => onDateSelected(entry.key),
+                          onTap: () => _onDateSelected(entry.key),
                           child: DateWidget(
                             dateItem: entry.value,
                             isSelected: selectedIndex == entry.key,
@@ -184,7 +170,7 @@ class DateSelectionBar extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.arrow_forward, color: Colors.white),
-            onPressed: () => scrollRight(),
+            onPressed: () => _scrollRight(),
           ),
         ],
       ),
@@ -211,37 +197,30 @@ class DateWidget extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 5.0),
       child: Column(
         children: [
+          Text(
+            dateItem.day,
+            style: TextStyle(
+              color: isSelected ? Colors.blue : Colors.grey,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(height: 5),
           Container(
-            width: 50,
-            height: 40,
+            width: 24,
+            height: 24,
             decoration: BoxDecoration(
               color: isSelected ? Colors.blue : Colors.transparent,
-              // shape: BoxShape.circle,
-              borderRadius: BorderRadius.circular(10),
+              shape: BoxShape.circle,
             ),
             child: Center(
-              child: Column(
-                children: [
-                  Text(
-                    dateItem.day,
-                    style: TextStyle(
-                      color: isSelected ? Colors.white : Colors.grey,
-                      fontWeight:
-                          isSelected ? FontWeight.bold : FontWeight.normal,
-                      fontSize: 12,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    dateItem.date,
-                    style: TextStyle(
-                      color: isSelected ? Colors.white : Colors.grey,
-                      fontWeight:
-                          isSelected ? FontWeight.bold : FontWeight.normal,
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
+              child: Text(
+                dateItem.date,
+                style: TextStyle(
+                  color: isSelected ? Colors.blue : Colors.grey,
+                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                  fontSize: 12,
+                ),
               ),
             ),
           ),
@@ -260,6 +239,266 @@ class DateWidget extends StatelessWidget {
     );
   }
 }
+
+// class HomeScreen extends StatefulWidget {
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//   late List<DateItem> dates;
+//   late int selectedIndex;
+//   String content = 'محتوى يوم';
+//   bool isLoading = false;
+//   bool isFirstLoad = true;
+//
+//   final ScrollController _scrollController = ScrollController();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     dates = _generateDatesForCurrentMonth();
+//     selectedIndex = DateTime.now().day - 1; // تحديد اليوم الحالي كمؤشر تلقائي
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       _scrollToSelectedIndex();
+//       _updateContent();
+//     });
+//   }
+//
+//   List<DateItem> _generateDatesForCurrentMonth() {
+//     List<DateItem> generatedDates = [];
+//     DateTime now = DateTime.now();
+//     int daysInMonth = DateTime(now.year, now.month + 1, 0).day;
+//
+//     for (int i = 1; i <= daysInMonth; i++) {
+//       DateTime date = DateTime(now.year, now.month, i);
+//       String dayName =
+//           DateFormat('EEEE', 'ar').format(date); // عرض اسم اليوم بالعربية
+//       generatedDates.add(DateItem(dayName, i.toString()));
+//     }
+//
+//     return generatedDates;
+//   }
+//
+//   void _onDateSelected(int index) {
+//     setState(() {
+//       isLoading = true;
+//       selectedIndex = index;
+//     });
+//
+//     // محاكاة عملية تحميل (مثل جلب بيانات من خادم)
+//     Future.delayed(Duration(seconds: 1), () {
+//       setState(() {
+//         isLoading = false;
+//         _updateContent();
+//         _scrollToSelectedIndex();
+//       });
+//     });
+//   }
+//
+//   void _updateContent() {
+//     setState(() {
+//       content =
+//           'محتوى يوم ${dates[selectedIndex].day} ${dates[selectedIndex].date}';
+//     });
+//   }
+//
+//   void _scrollToSelectedIndex() {
+//     if (isFirstLoad) {
+//       double position =
+//           selectedIndex * 60.0; // تحديد الموقع المناسب للتاريخ المحدد
+//       _scrollController.animateTo(
+//         position,
+//         duration: Duration(milliseconds: 300),
+//         curve: Curves.easeInOut,
+//       );
+//       isFirstLoad = false; // تعيين isFirstLoad إلى false بعد التمرير الأولي
+//     }
+//   }
+//
+//   void _scrollLeft() {
+//     _scrollController.animateTo(
+//       _scrollController.offset - 100,
+//       duration: Duration(milliseconds: 300),
+//       curve: Curves.easeInOut,
+//     );
+//   }
+//
+//   void _scrollRight() {
+//     _scrollController.animateTo(
+//       _scrollController.offset + 100,
+//       duration: Duration(milliseconds: 300),
+//       curve: Curves.easeInOut,
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       appBar: AppBar(
+//         title: Text('Date Selection Bar'),
+//         backgroundColor: Colors.black,
+//       ),
+//       body: Column(
+//         children: [
+//           DateSelectionBar(
+//             dates: dates,
+//             selectedIndex: selectedIndex,
+//             onDateSelected: _onDateSelected,
+//             scrollController: _scrollController,
+//             scrollLeft: _scrollLeft,
+//             scrollRight: _scrollRight,
+//           ),
+//           Expanded(
+//             child: Center(
+//               child: isLoading
+//                   ? CircularProgressIndicator()
+//                   : AnimatedSwitcher(
+//                       duration: Duration(milliseconds: 300),
+//                       transitionBuilder:
+//                           (Widget child, Animation<double> animation) {
+//                         return FadeTransition(opacity: animation, child: child);
+//                       },
+//                       child: Text(
+//                         content,
+//                         key: ValueKey<String>(content),
+//                         style: TextStyle(color: Colors.white, fontSize: 18),
+//                       ),
+//                     ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+// class DateSelectionBar extends StatelessWidget {
+//   final List<DateItem> dates;
+//   final int selectedIndex;
+//   final Function(int) onDateSelected;
+//   final ScrollController scrollController;
+//   final Function scrollLeft;
+//   final Function scrollRight;
+//
+//   DateSelectionBar({
+//     required this.dates,
+//     required this.selectedIndex,
+//     required this.onDateSelected,
+//     required this.scrollController,
+//     required this.scrollLeft,
+//     required this.scrollRight,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 20.0),
+//       child: Row(
+//         children: [
+//           IconButton(
+//             icon: Icon(Icons.arrow_back, color: Colors.white),
+//             onPressed: () => scrollLeft(),
+//           ),
+//           Expanded(
+//             child: SingleChildScrollView(
+//               scrollDirection: Axis.horizontal,
+//               controller: scrollController,
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: dates
+//                     .asMap()
+//                     .entries
+//                     .map((entry) => GestureDetector(
+//                           onTap: () => onDateSelected(entry.key),
+//                           child: DateWidget(
+//                             dateItem: entry.value,
+//                             isSelected: selectedIndex == entry.key,
+//                           ),
+//                         ))
+//                     .toList(),
+//               ),
+//             ),
+//           ),
+//           IconButton(
+//             icon: Icon(Icons.arrow_forward, color: Colors.white),
+//             onPressed: () => scrollRight(),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+// class DateItem {
+//   final String day;
+//   final String date;
+//
+//   DateItem(this.day, this.date);
+// }
+//
+// class DateWidget extends StatelessWidget {
+//   final DateItem dateItem;
+//   final bool isSelected;
+//
+//   DateWidget({required this.dateItem, required this.isSelected});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 5.0),
+//       child: Column(
+//         children: [
+//           Container(
+//             width: 50,
+//             height: 40,
+//             decoration: BoxDecoration(
+//               color: isSelected ? Colors.blue : Colors.transparent,
+//               // shape: BoxShape.circle,
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             child: Center(
+//               child: Column(
+//                 children: [
+//                   Text(
+//                     dateItem.day,
+//                     style: TextStyle(
+//                       color: isSelected ? Colors.white : Colors.grey,
+//                       fontWeight:
+//                           isSelected ? FontWeight.bold : FontWeight.normal,
+//                       fontSize: 12,
+//                     ),
+//                   ),
+//                   SizedBox(height: 5),
+//                   Text(
+//                     dateItem.date,
+//                     style: TextStyle(
+//                       color: isSelected ? Colors.white : Colors.grey,
+//                       fontWeight:
+//                           isSelected ? FontWeight.bold : FontWeight.normal,
+//                       fontSize: 12,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           SizedBox(height: 5),
+//           if (isSelected)
+//             Container(
+//               width: 5,
+//               height: 5,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue,
+//                 shape: BoxShape.circle,
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 // class HomeScreen extends StatefulWidget {
 //   @override
